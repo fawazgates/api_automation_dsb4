9341013d075b2d7861511812c9af1a6b5a67433c
fawaz <fawaz.ajja@gmail.com>
2024-06-19 13:57:38 +0700

    update schema

--- tests/apiTests.js
+++ tests/apiTests.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiJsonSchema = require('chai-json-schema');
 const supertest = require('supertest');
+const userSchema = require('./schema'); // Import the schema
 
 chai.use(chaiJsonSchema);
 
@@ -8,33 +9,6 @@ const { expect } = chai;
 
 const BASE_URL = 'https://reqres.in/api';
 const request = supertest(BASE_URL);
-
-// Schema Definitions
-const userSchema = {
-  type: 'object',
-  required: ['data', 'support'],
-  properties: {
-    data: {
-      type: 'object',
-      required: ['id', 'email', 'first_name', 'last_name', 'avatar'],
-      properties: {
-        id: { type: 'integer' },
-        email: { type: 'string' },
-        first_name: { type: 'string' },
-        last_name: { type: 'string' },
-        avatar: { type: 'string' },
-      },
-    },
-    support: {
-      type: 'object',
-      required: ['url', 'text'],
-      properties: {
-        url: { type: 'string' },
-        text: { type: 'string' },
-      },
-    },
-  },
-};
 
 describe('Reqres API Automation Tests', () => {
   it('Sample GET Request', async () => {
